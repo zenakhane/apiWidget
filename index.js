9fa112a39f2402153fafe21cc976b9c5e40c93c6
zenakhane <lilliantyiso@gmail.com>
2022-03-29 16:08:15 +0200

    first time

---
+++ index.js
@@ -0,0 +1,19 @@
+const carsElem = document.querySelector(".carsDisplay")
+
+axios
+.get("http://api-tutor.herokuapp.com/v1/cars")
+.then(function(cars){
+console.log(cars.data);
+
+cars.data.forEach(element => {
+    const li = document.createElement("tr");
+    li.innerHTML = `
+    <td>${element.make} <strong></td>
+    <td>${element.model}</strong></td>
+    <td> <small>${element.color}</small> </td>
+`
+    carsElem.appendChild(li)
+});
+
+
+}); 
